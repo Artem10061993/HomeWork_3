791319e31c2ed2d7955f0e7be54d24a66a9aa6ac
Artem Kandakov <Temofey1993@gmail.com>
2022-08-17 22:48:45 +0500

    Задача №23

---
+++ Exercise_23/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите целое число");
+try{
+    int x=Convert.ToInt32(Console.ReadLine());
+    int i=1;
+    while(i<=x)
+    {
+        if(i==x)
+        {
+        double z = Math.Pow(i,3);
+        Console.Write(z);
+        }
+        else
+        {
+        double z = Math.Pow(i,3);
+        Console.Write(z+",");
+
+        }
+        i++;
+    }
+}
+catch
+{
+    Console.WriteLine("Введте корректные данные");
+}
